1b729086a260e5e48eeeda4b9a2b35dab20ed649
brydiaz <bryandiaz13301@gmail.com>
2021-03-16 13:18:16 -0600

    4InLine listo

--- Code/4InLine.c
+++ Code/4InLine.c
@@ -2,75 +2,114 @@
 #include <stdlib.h>
 #include <string.h>
 #include "4InLine.h"
-
-
 #define N 7
 #define VALOR_VICTORIA 1024
 #define FICHA_CPU 'X'
 #define FICHA_JUGADOR 'O'
+#define ANSI_COLOR_BLUE    "\x1b[34m"
+#define ANSI_COLOR_MAGENTA "\x1b[35m"
+#define ANSI_COLOR_RESET   "\x1b[0m"
 
-/*
-Entradas: el tablero que se desea imprimir
-Salidas: el tablero impreso
 
-imprime el tablero
-*/
-void printTable(char table[7][7]){
+
+void imprimirtablero(char tablero[7][7]){
     printf(" 0\t 1\t 2\t 3\t 4\t 5\t 6\t\n");
     for(int i = 0; i<7; i++){
         for (int j = 0; j<7; j++){
-            printf("|%c| \t", table[i][j]);
+			if(tablero[i][j] == FICHA_JUGADOR){
+            	printf(ANSI_COLOR_BLUE  "|%c| \t" ANSI_COLOR_RESET, tablero[i][j]);
+
+			}else if(tablero[i][j] == FICHA_CPU){
+            	printf(ANSI_COLOR_MAGENTA  "|%c| \t" ANSI_COLOR_RESET, tablero[i][j]);
+
+			}else{
+            	printf("|%c| \t", tablero[i][j]);
+			}
         }
         printf("\n");
-    }
-    return 0;
+    }    
+}
+
+void arteAscii(int op){
+	
+	if(op == 1){
+		printf("  _  _                  _  __                 \n");
+		printf(" | || |                |  /_/                 \n");
+		printf(" | || |_    ___ _ __   | |_ _ __   ___  __ _ \n");
+		printf(" |__   _|  / _ %c '_ %c  | | | '_ %c / _ %c%c _` |\n", 92, 92, 92, 92, 47);
+		printf("    | |   |  __/ | | | | | | | | |  __/ (_| |\n");
+		printf("    |_|    %c___|_| |_| |_|_|_| |_|%c___|%c__,_|\n", 92, 92, 92);
+	}else if(op == 2){
+		printf("+-+-+ +-+-+-+-+-+\n");
+		printf("|1|.| |J|U|G|A|R|\n");
+		printf("+-+-+ +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+\n");
+		printf("|2|.| |D|E|S|A|R|R|O|L|L|A|D|O|R|E|S|\n");
+		printf("+-+-+ +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+\n");
+		printf("|3|.| |S|A|L|I|R|\n");
+		printf("+-+-+ +-+-+-+-+-+\n");
+
+	}else if(op == 3){
+		printf(" / \ / \ / \ / \   / \ / \ / \ / \ / \ / \ / \ / \ / \   / \ / \ / \ / \ / \ / \ / \   / \ / \ / \ / \ / \ / \ / \ \n");
+		printf("( J | o | s | e ) ( A | l | e | x | a | n | d | e | r ) ( A | r | t | a | v | i | a ) ( Q | u | e | s | a | d | a )\n");
+		printf(" \_/ \_/ \_/ \_/   \_/ \_/ \_/ \_/ \_/ \_/ \_/ \_/ \_/   \_/ \_/ \_/ \_/ \_/ \_/ \_/   \_/ \_/ \_/ \_/ \_/ \_/ \_/ \n");
+		printf("\n");
+
+		printf(" / \ / \ / \ / \ / \   / \ / \ / \ / \ / \ / \   / \ / \ / \ / \   / \ / \ / \ / \ / \ / \ / \ / \ / \ / \ / \ \n");
+		printf("( B | r | y | a | n ) ( A | n | d | r | e | y ) ( D | i | a | z ) ( B | a | r | r | i | e | n | t | o | s )\n");
+		printf(" \_/ \_/ \_/ \_/ \_/   \_/ \_/ \_/ \_/ \_/ \_/   \_/ \_/ \_/ \_/   \_/ \_/ \_/ \_/ \_/ \_/ \_/ \_/ \_/ \_/ \n");
+		printf("\n");
+
+		printf("  / \ / \ / \ / \ / \   / \ / \ / \ / \ / \ / \ / \   / \ / \ / \ / \ / \ / \ / \ / \ / \   / \ / \ / \ / \ \n");
+		printf(" ( J | o | s | u | e ) ( G | e | r | a | r | d | o ) ( G | u | t | i | e | r | r | e | z ) ( M | o | r | a )\n");
+		printf("  \_/ \_/ \_/ \_/ \_/   \_/ \_/ \_/ \_/ \_/ \_/ \_/   \_/ \_/ \_/ \_/ \_/ \_/ \_/ \_/ \_/   \_/ \_/ \_/ \_/ \n");
+		printf("\n");
+	}else if(op == 4){
+
+		printf("*	*	*	*	*	*	*	*	*	*	*	*	*	*	*	*    \n");
+		printf("  *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *  \n");
+		printf("*	*	*	*	*	*	*	*	*	*	*	*	*	*	*	*	 \n");
+		printf("  *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *  \n");
+		printf("*	*	*	*	*	*	*	*	*	*	*	*	*	*	*	*	*\n");
+		printf("  *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *  \n");
+		printf("*	*	*	*	*	*	*	*	*	*	*	*	*	*	*	*	*\n");
+		printf("  *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *  \n");
+	
+
+
+	}else if(op == 5){
+		printf("+-+-+-+-+-+-+ +-+-+-+ +-+-+-+-+-+-+\n");
+		printf("|D|I|G|I|T|A| |U|N|A| |O|P|C|I|O|N|}\n");
+		printf("+-+-+-+-+-+-+ +-+-+-+ +-+-+-+-+-+-+\n");
+
+	}
     
 }
 
-/*
-Entradas: --
-Salidas: --
+void jugarDeNuevo(){
 
-imprime ascii code
+	int op;
+	printf("¿Desea jugar de nuevo? 1. Sí 2. No");
+	scanf("%d", &op);
+	if(op == 1){
+		juegoPrincipal();
+	}else{
+		printf("Gracias por usar :D");
+		exit(-1);
+	}
 
-*/
-
-void asciiArt(){
-
-    printf("  _  _                  _  __                 \n");
-    printf(" | || |                |  /_/                 \n");
-    printf(" | || |_    ___ _ __   | |_ _ __   ___  __ _ \n");
-    printf(" |__   _|  / _ %c '_ %c  | | | '_ %c / _ %c%c _` |\n", 92, 92, 92, 92, 47);
-    printf("    | |   |  __/ | | | | | | | | |  __/ (_| |\n");
-    printf("    |_|    %c___|_| |_| |_|_|_| |_|%c___|%c__,_|\n", 92, 92, 92);
-
-    
 }
 
-/*
-Entradas: el tablero que se usará como referencia, el movimiento que se jugó
-y la ficha que se está jugando
-
-Salidas: --
-
-Pone el movimiento deseado en el tablero
-
-*/
-
-void makeMove(char table[7][7], int move, char token){
+void hacerMovimiento(char tablero[7][7], int move, char token){
 
     for(int i = 6; i>-1; i--){
-        if(table[i][move] == ' '){
-            table[i][move] =  token;
+        if(tablero[i][move] == ' '){
+            tablero[i][move] =  token;
             break;
         }
     }
 
 }
 
-
-//Se le pasa el tablero para que le dé la cantidad de cuantos hijos
-//puede tener según la cantidad de columnas con la primera fila vacía
 int posibilidades(char tablero[N][N])
 {
 	int j;
@@ -283,8 +322,8 @@ int comprobarVictoriaJugador(char tablero[N][N])
 
 	return 0;
 }
-//Cada vez que el
-double Heuristica(char tablero[N][N])
+
+double heuristica(char tablero[N][N])
 {
 	if (comprobarVictoriaPC(tablero))
 		return VALOR_VICTORIA;
@@ -294,8 +333,7 @@ double Heuristica(char tablero[N][N])
 		return 0;
 }
 
-//Hacer una copia del tablero para que así el minimax pueda generar un juego teórico por aparte
-//y así elegir lo que crea que es la mejor opción
+
 void tableroTeorico(char tablero1[N][N], char tablero2[N][N]){
 	int i; int j;
 	for(i=0;i<N;i++){
@@ -305,9 +343,7 @@ void tableroTeorico(char tablero1[N][N], char tablero2[N][N]){
 	}
 }
 
-
-//Se encarga de crear un Nodo y cargarlo con nivel, n_hijos, valor, movimientos
-Nodo CreaNodo(char tablero[N][N],int nivel,int profundidadArbol){
+Nodo crearNodo(char tablero[N][N],int nivel,int profundidadArbol){
 	int i; int j=0;
 	Nodo p;
 
@@ -359,8 +395,7 @@ Nodo CreaNodo(char tablero[N][N],int nivel,int profundidadArbol){
 
 
 
-// Crea nodos hijos y los carga con a info que tiene cada nodo (n_hijos, movimientos, nivel, ...) creando así un nivel más en el árbol
-void CreaNivel(Nodo *Padre,char ficha,int profundidadArbol){
+void crearNivel(Nodo *Padre,char ficha,int profundidadArbol){
 	int i;
 	Nodo *Hijo;
 
@@ -372,10 +407,10 @@ void CreaNivel(Nodo *Padre,char ficha,int profundidadArbol){
 		tableroTeorico(Padre->tablero,Hijo->tablero);
 
 		//Envia la ficha y la mete en el tablero del NODO HIJO según la columna que vaya en el for
-		makeMove(Hijo->tablero,Padre->movimientos[i],ficha);
+		hacerMovimiento(Hijo->tablero,Padre->movimientos[i],ficha);
 
 		//Carga el Nodo Hijo
-		*Hijo = CreaNodo(Hijo->tablero,Padre->nivel+1,profundidadArbol);
+		*Hijo = crearNodo(Hijo->tablero,Padre->nivel+1,profundidadArbol);
 
 		//Carga al nodo padre con un nodo (hoja) que funciona cómo opcion
 		//de lo que puede ser una jugada
@@ -384,14 +419,12 @@ void CreaNivel(Nodo *Padre,char ficha,int profundidadArbol){
 }
 
 
-//raiz: El nodo por el que comienza
-//profundidad: Hasta donde llegara el árbol (no puede ser infinito por lo que se le da una un número especifico que se va reduciendo)
-void CreaArbol(Nodo *raiz, int profundidad, int profundidadArbol){
+void crearArbol(Nodo *raiz, int profundidad, int profundidadArbol){
 
 	//Se encarga de crear nodos hijos y cargarlos, esto según la cantidad de hijos que tenga la raiz
-	CreaNivel(raiz, FICHA_CPU, profundidadArbol);
+	crearNivel(raiz, FICHA_CPU, profundidadArbol);
 	for (int i = 0; i < raiz->n_hijos; i++){
-		CreaNivel(raiz->hijos[i], FICHA_JUGADOR, profundidadArbol);
+		crearNivel(raiz->hijos[i], FICHA_JUGADOR, profundidadArbol);
 	}
 
 	//Marca el fin de la recursividad
@@ -407,24 +440,21 @@ void CreaArbol(Nodo *raiz, int profundidad, int profundidadArbol){
 			// En la matriz hijos, se le debe de dar a cada hijo otros
 			// hijos para así ir generando el árbol
 			for(j=0; j < raiz->hijos[i]->n_hijos  ;j++){
-				CreaArbol(raiz->hijos[i]->hijos[j],profundidad-1,profundidadArbol);
+				crearArbol(raiz->hijos[i]->hijos[j],profundidad-1,profundidadArbol);
 			}
 		}
 	}
 	return;
 }
 
-
-//Sirve para liberar memoria o tarde o temporano el programa colapsaría
-//va eliminando nodo por nodo con recursividad
-void BorraArbol(Nodo *raiz){
+void borrarArbol(Nodo *raiz){
 	if(raiz->n_hijos == 0){
         free(raiz);
 	}
 	else{
         int i;
         for (i=0;i<raiz->n_hijos;i++){
-            BorraArbol(raiz->hijos[i]);
+            borrarArbol(raiz->hijos[i]);
         }
         free(raiz->hijos);
         free(raiz->movimientos);
@@ -433,10 +463,7 @@ void BorraArbol(Nodo *raiz){
 	return;
 }
 
-
-
-//Se le da un valor a cada hoja para así poder
-void ValorarHojas(Nodo *raiz){
+void valorarHojas(Nodo *raiz){
 
 	//Se encuentra en el nivel donde el nodo analizado ya no tiene más niveles
 	if (raiz->n_hijos == 0){
@@ -446,31 +473,18 @@ void ValorarHojas(Nodo *raiz){
 		//recuerde que está usando un tablero "copia" el cual mediante "crearArbol" se le cargaron posibles
 		//lugares donde puede dejar caer la ficha, el tablero que pasamos en heuristica lleva esa jugada "teórica"
 		//así es cómo puede comprobar si la máquina "ganará" en teoría.
-		raiz->valor = Heuristica(raiz->tablero);
+		raiz->valor = heuristica(raiz->tablero);
 	}
 
 	//Continuar bajando hata llegar a la última fila de nodos (último nivel)
 	else{
 		for (int i=0;i<raiz->n_hijos;i++){
-			ValorarHojas(raiz->hijos[i]);
+			valorarHojas(raiz->hijos[i]);
 		}
 	}
 }
 
-
-
-
-
-//	Función principal de Minimax
-/*
-	Parte de la teoría de Minimax indica que los niveles van salteados entre máximo y mínimo
-	de ahí que se analice "Raiz->nivel % 2  == 0" para los valores máximos 
-	y
-	"Raíz->nivel %2 == 1" para los valores mínimos
-
-	por esto mismo es que se llama Minimax
-*/
-void MiniMax(Nodo *Raiz){
+void minimax(Nodo *Raiz){
 	// Esta es una ligera variante del minimax, donde las herencias se dividen entre 2
 
 	// Comprueba si ya se llegó a lo más bajo del arbolito
@@ -479,10 +493,10 @@ void MiniMax(Nodo *Raiz){
 		//Recorre los hijos del nodo en cuestión
         	for (int i=0; i<Raiz->n_hijos ;i++){
 
-			//Si tiene hijos, los recorre con el Minimax bajando al siguiente nivel hasta llegar al último
+			//Si tiene hijos, los recorre con el minimax bajando al siguiente nivel hasta llegar al último
 			// y así cumplir con la recursividad
             	if (Raiz->hijos[i]->n_hijos != 0){
-                		MiniMax(Raiz->hijos[i]);
+                		minimax(Raiz->hijos[i]);
             	}
         	}
 
@@ -526,26 +540,21 @@ void MiniMax(Nodo *Raiz){
 	}
 }
 
-
-
-//La computadora está jugando
-//Se le pasa el tablero y se le pasa la profundidadArbol
-//Hay una matriz que lleva las varas lógicas
 int juegaPC(char tablero[N][N], int profundidadArbol){
 
 	//Crea un nodo raiz que se encarga de tener hijos, movimientos, nivel?, valor? y copiar la matriz
-	Nodo Raiz = CreaNodo(tablero,0,profundidadArbol);
+	Nodo Raiz = crearNodo(tablero,0,profundidadArbol);
 
 
 	//Crea el arbol de posibilidades
-	CreaArbol(&Raiz,profundidadArbol,profundidadArbol);
+	crearArbol(&Raiz,profundidadArbol,profundidadArbol);
 
 	// Sirve para ir dándole valores de "gane teórico" a los hijos de la raíz
-	ValorarHojas(&Raiz);
+	valorarHojas(&Raiz);
 
 
 	//Algoritmo principal que se va a encargar de tomar las decisiones
-	MiniMax(&Raiz);
+	minimax(&Raiz);
 
 
 
@@ -574,7 +583,7 @@ int juegaPC(char tablero[N][N], int profundidadArbol){
 
 
 	//Se libera la memoria o el programa explota
-	BorraArbol(&Raiz);
+	borrarArbol(&Raiz);
 
 
 
@@ -582,187 +591,91 @@ int juegaPC(char tablero[N][N], int profundidadArbol){
 	return movimiento;
 }
 
-/*
-Entradas: el tablero que se usa como referencia, el la ficha del jugador
-Salidas: 1 si se ganó
+void juego(){
 
-Verifica horizontal, vertical, o diagonales si gana
-
-*/
-int checkVictory(char table[7][7], char token){
-	int i;      //fila
-      int j;      //columna
-
-
-
-
-	// Comprobación horizontal
-
-	//va analizando fila por fila
-	for(i=0;i<7;i++){
-
-		//Esta analiza la parte horizontal, termina en 7-3 porque 
-		//faltando 3 columnas ya no es necesario analizar nada, es imposible que haya 4 en línea en solo 3 espacios
-		for(j=0;j<(7-3);j++){
-
-			//Comienza a comprobar si las 4 están en fila horizontal
-			if(	(table[i][j] == token) &&
-				(table[i][j+1] == token) &&
-				(table[i][j+2] == token) &&
-				(table[i][j+3] == token)){	
-					//retorna 1 en caso de haber ganado
-					return 1;
-				}
-		}
-
-	}
-	
-
-	// Comprobación vertical
-	for(j=0;j<7;j++){
-
-		//Esta analiza la parte vertical, termina en 7-3 porque
-		//faltando 3 filas ya no es necesario analizar nada, es imposible que haya 4 en línea en solo 3 espacios
-		for(i=0;i<(7-3);i++){
-
-			//Comienza a comprobar si las 4 están en fila vertical
-			if(	(table[i][j] == token) &&
-				(table[i+1][j] == token) &&
-				(table[i+2][j] == token) &&
-				(table[i+3][j] == token)){
-					return 1;
-				}
-		}
-
-	}
-
-
-
-	// Comprobación diagonal de izquierda a derecha es decir -> \
-	//Es una diagonal, a partir de la fila 7-3 es imposible que hayan 4 en línea
-	for(i=0;i<(7-3);i++){
-
-		//es una diagonal, a partir de la columna 7-3 es imposible que hayan 4 en línea
-		for(j=0;j<(7-3);j++){
-
-
-			//comienza a comprobar si las 4 están en fila diagonal
-			//La comprobación va sumando en cada if porque es diagonal
-			if(	(table[i][j] == token) && 
-				(table[i + 1][j + 1] == token) && 
-			   	(table[i + 2][j + 2] == token) && 
-				(table[i + 3][j + 3] == token)){
-				return 1;
-			}
-		}
-	}
-
-
-	// Comprobación diagonal de derecha a izquierda es decir -> /
-
-	//Comienza en 7-3 porque faltando 3 filas ya no es necesario analizar nada
-	//es imposible que haya 4 en línea en solo 3 espacios
-	for(i=0;i<(7-3);i++){
-	
-		//Esta es diferente porque comienza por la columna derecha y va restando,
-		//Debe ser mayor a 2, porque las últimas diagonales solo tiene 3, 2 y 1 espacio respectivamente
-		for(j=7-1;j>2;j--){
-
-			//Aqui comienza a comprobar si las 4 tokens están en diagonal /
-			//Se van sumando las filas (analizando hacia abajo) y restando las columnas (analizando hacia la derecha)
-			if(	(table[i][j] == token) &&
-				(table[i+1][j-1] == token) &&
-				(table[i+2][j-2] == token) &&
-				(table[i+3][j-3] == token)){
-							return 1;
-				}
-		}
-	}
-
-	return 0;
-}
-
-/*
-Entradas: --
-Salidas: --
-
-Controla el flujo del programa 
-cuando juega el jugador y cuando juega el pc
-*/
-
-void gameOn(){
-
-    int winner = 0;
-    int turn = 0;
-    int finalWinner = 0;
-    char table[7][7];
+    int ganador = 0;
+    int turno = 0;
+    int ganadorFinal = 0;
+    char tablero[7][7];
 
     for(int i = 0; i<7; i++){
         for (int j = 0; j<7; j++){
-            table[i][j] = ' ';
+            tablero[i][j] = ' ';
         }
     }
 
-    while(winner == 0){
-        printTable(table);
-        if(turn == 0){
+    while(ganador == 0){
+		system("clear");
+		arteAscii(4);
+        imprimirtablero(tablero);
+		
+        if(turno == 0){
 
             int op;
             printf("\n");
-            printf("DIGITA UNA OPCION JUGADOR\n");
+            arteAscii(5);
+			arteAscii(4);
             scanf("%d", &op);
-            makeMove(table, op, FICHA_JUGADOR);
-            winner = checkVictory(table, FICHA_JUGADOR);
-            if(winner == 1){
-                finalWinner = 1;
+            hacerMovimiento(tablero, op, FICHA_JUGADOR);
+            ganador = comprobarVictoriaJugador(tablero);
+            if(ganador == 1){
+                ganadorFinal = 1;
             }
-            turn = 1;
+            turno = 1;
         }else{
 
-	        int columna = juegaPC(table, 2);
-	        makeMove(table,columna,FICHA_CPU);
-            winner = checkVictory(table, FICHA_CPU);
-            if(winner == 1){
-                finalWinner = 0;
+	        int columna = juegaPC(tablero, 2);
+			arteAscii(4);
+	        hacerMovimiento(tablero,columna,FICHA_CPU);
+            ganador = comprobarVictoriaPC(tablero);
+            if(ganador == 1){
+                ganadorFinal = 0;
             }
-            turn = 0;
+            turno = 0;
         
 
         }
     }
 
-    if(finalWinner == 1){
-        printf("FELICIDADES LE GANASTE AL IA");
+    if(ganadorFinal == 1){
+		arteAscii(4);
+        printf("FELICIDADES LE GANASTE AL IA\n");
+		jugarDeNuevo();
+		arteAscii(4);
     }else{
-        printf("QUE MAL, PERDISTE CONTRA EL IA");
+		arteAscii(4);
+        printf("QUE MAL, PERDISTE CONTRA EL IA\n");
+		jugarDeNuevo();
+		arteAscii(4);
     }
 }
 
-
-/*
-Entradas: --
-Salidas: --
-
-Control del menú en el programa
-
-*/
-
-void mainGame(){
-    asciiArt();
+void juegoPrincipal(){
+	printf("x\n");
+	system("clear");
+    arteAscii(1);
     int op; 
     printf("\n");
-    printf("1. Iniciar juego\n");
-    printf("2. Desarrolladores\n");
-    printf("3. Salir\n");
+    arteAscii(2);
     scanf("%d", &op);
 
     if(op == 1 ){
 
-        gameOn();
+        juego();
 
     }else if(op == 2){
-        
-        printf("Developers");
+        system("clear");
+        arteAscii(3);
+		printf("\n");
+    	printf("VOLVER A MENÚ: 1. Sí 2. No\n");
+    	scanf("%d", &op);
+		if(op == 1){
+			juegoPrincipal();
+		}else{
+			printf("GRACIAS POR USAR :D");
+			exit(-1);
+		}
+		
 
     }else if(op == 3){
         printf("GRACIAS POR USAR :D");
@@ -770,14 +683,18 @@ void mainGame(){
 
     }else{
         printf("ERROR Opcion invalida\n");
-        mainGame();
+        juegoPrincipal();
     }
     
     
 }
 
 
+
+
+
+
 int main(){
-    mainGame();
+    juegoPrincipal();
     return 0;
 }
---
+++ Code/4InLine.h
@@ -0,0 +1,217 @@
+#define N 7
+#define VALOR_VICTORIA 1024
+#define FICHA_CPU 'X'
+#define FICHA_JUGADOR 'O'
+
+
+//Struct de nodo para poder crear los arboles de minimax
+typedef struct nodo{
+	struct nodo **hijos;
+	int *movimientos;
+	int n_hijos;
+	char tablero[N][N];
+	double valor;
+	int nivel;
+} Nodo;
+
+//-------------------------------------------------------------------------------
+
+/*
+Entradas: el tablero que se usará como referencia
+
+Salidas: --
+
+Se le pasa el tablero para que le dé la cantidad de cuantos hijos
+puede tener según la cantidad de columnas con la primera fila vacía
+
+*/
+int posibilidades(char[N][N]);
+//-------------------------------------------------------------------------------
+
+
+//-------------------------------------------------------------------------------
+
+/*
+Entradas: el tablero que se usa como referencia
+Salidas: 1 si se ganó
+Verifica horizontal, vertical, o diagonales si gana
+*/
+int comprobarVictoriaPC(char partida[N][N]);
+int comprobarVictoriaJugador(char partida[N][N]);
+//-------------------------------------------------------------------------------
+
+//-------------------------------------------------------------------------------
+
+double heuristica(char[N][N]);
+//-------------------------------------------------------------------------------
+
+
+//-------------------------------------------------------------------------------
+/*
+Entradas: el tablero que se usa como referencia, el tablero que copia
+Salidas: 1--
+Hacer una copia del tablero para que así el minimax pueda generar un juego teórico por aparte
+/y así elegir lo que crea que es la mejor opción
+
+*/
+void tableroTeorico(char [N][N],char [N][N]);
+//-------------------------------------------------------------------------------
+
+
+//-------------------------------------------------------------------------------
+
+/*
+Entradas: el tablero que se usa como referencia, el nivel, y su profundidad 
+Salidas: --
+Se encarga de crear un Nodo y cargarlo con nivel, n_hijos, valor, movimientos
+*/
+
+Nodo creaNodo(char [N][N],int ,int );
+//-------------------------------------------------------------------------------
+
+
+//-------------------------------------------------------------------------------
+/*
+
+Entradas: puntero a un nodo padre, la ficha del jugador, y la profundidad del arbol
+salidas: --
+// Crea nodos hijos y los carga con a info que tiene cada nodo (n_hijos, movimientos, nivel, ...) creando así un nivel más en el árbol
+
+*/
+void creaNivel(Nodo *,char ,int );
+//-------------------------------------------------------------------------------
+
+/*
+//-------------------------------------------------------------------------------
+
+Entradas: Puntero de un nodo p
+salidas: --
+
+*/
+void creaDobleNivel(Nodo *,int );
+//-------------------------------------------------------------------------------
+
+
+/*
+---------------------------------------------------------------------------
+Entradas:recibe un nodo raiz, la profundidad y la profundidad del arbol
+salidas: --
+
+Crea el arbol de llamadas recursivas y sus niveles
+*/
+void creaArbol(Nodo *,int ,int );
+//-------------------------------------------------------------------------------
+
+//-------------------------------------------------------------------------------
+/*
+Entradas:recibe un nodo raiz
+salidas: --
+
+borra el arbol hoja a hoja en llamadas recursivas 
+*/
+void borrarArbol(Nodo *);
+//-------------------------------------------------------------------------------
+
+//-------------------------------------------------------------------------------
+/*
+Entradas:recibe un nodo raiz
+salidas: --
+
+le valor a las hojas para poder trabajarlas
+*/
+void valorarHojas(Nodo *);
+//-------------------------------------------------------------------------------
+
+//-------------------------------------------------------------------------------
+/*
+
+Entradas:recibe un nodo raiz
+salidas: --
+
+Crea el algoritmo miniMax
+Parte de la teoría de minimax indica que los niveles van salteados entre máximo y mínimo
+de ahí que se analice "Raiz->nivel % 2  == 0" para los valores máximos 
+y
+"Raíz->nivel %2 == 1" para los valores mínimos
+
+por esto mismo es que se llama minimax
+*/
+void miniMax(Nodo *);
+//-------------------------------------------------------------------------------
+
+//-------------------------------------------------------------------------------
+/*
+
+Entradas:recibe un tablero, y la profundidad del arbol
+salidas: Un entero que representa la columna a jugar
+se encarga de jugar el turno de la pc, inicar el minimax etc
+
+*/
+int juegaPC(char [N][N],int );
+//-------------------------------------------------------------------------------
+
+//-------------------------------------------------------------------------------
+/*
+Entradas: el tablero que se desea imprimir
+Salidas: el tablero impreso
+
+imprime el tablero
+*/
+void imprimitTablero(char tablero[7][7]);
+//-------------------------------------------------------------------------------
+
+//-------------------------------------------------------------------------------
+
+/*
+---------------------------------------------------------------------------
+Entradas:--
+salidas: --
+
+Flujo de juego principal
+*/
+void juegoPrincipal();
+//-------------------------------------------------------------------------------
+
+//-------------------------------------------------------------------------------
+/*
+Entradas: El ascii que desea implementar
+Salidas: --
+
+imprime ascii code
+
+*/
+void arteAscii(int op);
+//-------------------------------------------------------------------------------
+
+//-------------------------------------------------------------------------------
+/*
+Entradas: el tablero que se usará como referencia, el movimiento que se jugó
+y la ficha que se está jugando
+
+Salidas: --
+
+Pone el movimiento deseado en el tablero
+
+*/
+void hacerMovimiento(char tablero[7][7], int movimiento, char ficha);
+//-------------------------------------------------------------------------------
+
+//-------------------------------------------------------------------------------
+/*
+Entradas: --
+Salidas: --
+Verifica si desea jugar de nuevo
+*/
+void jugarDeNuevo();
+//-------------------------------------------------------------------------------
+
+//-------------------------------------------------------------------------------
+/*
+---------------------------------------------------------------------------
+Entradas:--
+salidas: --
+
+Flujo de juego 
+*/
+void juego();
+//-------------------------------------------------------------------------------
