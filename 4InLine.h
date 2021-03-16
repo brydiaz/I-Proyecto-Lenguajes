2b013ffa2bb3979c5f3b40d5da727f219a9424b8
brydiaz <bryandiaz13301@gmail.com>
2021-03-16 00:51:12 -0600

    Juego listo, falta Minimax

---
+++ 4InLine.h
@@ -0,0 +1,8 @@
+
+
+void printTable(char table[7][7]);
+void mainGame();
+void asciiArt();
+void makeMove(char table[7][7], int move, char token);
+int checkVictory(char table[7][7], char token);
+void gameOn();
